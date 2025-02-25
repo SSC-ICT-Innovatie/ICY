0eb2f5a03e589466610f33ccabe10e8230dc1c64
Liam Plokkaar <l.plokkaar11@gmail.com>
2025-02-25 19:59:20 +0100

    commit test branch 5

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World App'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
