47441ea71864984babb0e4c0f072f0f1c5790ab5
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-02-27 14:06:38 +0100

    cleanup

--- client/icy/lib/abstractions/utils/constants.dart
+++ client/icy/lib/abstractions/utils/constants.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/material.dart';
-
 class AppConstants {
   // App general settings
   static const String appName = "ICY";
   static const double defaultPadding = 16.0;
-  
+
   // Navigation constants
   static const int defaultNavigationIndex = 0;
 }
--- client/icy/lib/dependency_injector.dart
+++ client/icy/lib/dependency_injector.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:icy/abstractions/navigation/state/navigation_cubit.dart';
 import 'package:icy/icy_tab_registerar.dart';
@@ -6,12 +5,10 @@ import 'package:icy/icy_tab_registerar.dart';
 class DependencyInjector {
   static Future<List<BlocProvider>> getProviders() async {
     final tabs = await injectNavigationTabs();
-    
+
     return [
-      BlocProvider<NavigationCubit>(
-        create: (context) => NavigationCubit(tabs),
-      ),
-      // Add other providers as needed
+      BlocProvider<NavigationCubit>(create: (context) => NavigationCubit(tabs)),
+      // other providers can come here
     ];
   }
 }
