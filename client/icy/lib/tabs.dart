5bac8785fa7b0c0e6e3b217b9f7477c85ac3c5b9
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-02-27 14:22:59 +0100

    removed unnecessary platforms (cleanup)

--- client/icy/lib/tabs.dart
+++ client/icy/lib/tabs.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
 import 'package:icy/abstractions/navigation/services/icy_tab_registerar.dart';
 
 /// Register all navigation tabs here
@@ -5,23 +7,31 @@ import 'package:icy/abstractions/navigation/services/icy_tab_registerar.dart';
 Future<List<IcyTab>> injectNavigationTabs() async {
   return [
     // Example:
-    // IcyTab(
-    //   icon: FAssets.icons.house,
-    //   title: "Home",
-    //   content: Container(color: Colors.pink),
-    //   // Optional auth rule example:
-    //   // accessRule: () => AuthService.isLoggedIn,
-    //   // fallbackContent: LoginPage(),
-    // ),
+    IcyTab(
+      icon: FAssets.icons.house,
+      title: "Home",
+      content: Container(color: Colors.pink),
+      // Optional auth rule example:
+      // accessRule: () => AuthService.isLoggedIn,
+      // fallbackContent: LoginPage(),
+    ),
 
-    // IcyTab(
-    //   icon: FAssets.icons.search,
-    //   title: "Home",
-    //   content: Container(color: Colors.blue),
-    //   // Optional auth rule example:
-    //   // accessRule: () => AuthService.isLoggedIn,
-    //   // fallbackContent: LoginPage(),
-    // ),
+    IcyTab(
+      icon: FAssets.icons.search,
+      title: "Search",
+      content: Container(color: Colors.blue),
+      // Optional auth rule example:
+      // accessRule: () => AuthService.isLoggedIn,
+      // fallbackContent: LoginPage(),
+    ),
+        IcyTab(
+      icon: FAssets.icons.testTube,
+      title: "Test",
+      content: Container(color: Colors.orange),
+      // Optional auth rule example:
+      // accessRule: () => AuthService.isLoggedIn,
+      // fallbackContent: LoginPage(),
+    ),
     // Add more tabs as needed
   ];
 }
