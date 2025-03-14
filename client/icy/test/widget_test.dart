c66c5db75c4879044d1b60dc3e872ffb2eebab26
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-03-14 17:38:42 +0100

    Delete widget_test.dart

--- client/icy/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Placeholder test', (WidgetTester tester) async {
-    // This is a placeholder test to replace the counter example
-    expect(true, isTrue);
-  });
-}
