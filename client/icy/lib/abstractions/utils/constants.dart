94763a75c990a3994ed7edb7db09a635f4bbe537
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-04-02 02:56:27 +0200

    added dark theme key value pair bindings for shared prefs

--- client/icy/lib/abstractions/utils/constants.dart
+++ client/icy/lib/abstractions/utils/constants.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppConstants {
+  static final AppConstants _instance = AppConstants._internal();
+  factory AppConstants() => _instance;
+  AppConstants._internal();
+
+  // Keys for SharedPreferences
+  static const String isDarkModeKey = 'isDarkMode';
+  static const String useSystemThemeKey = 'useSystemTheme';
+
   // App general settings
   static const String appName = "ICY";
   static const double defaultPadding = 16.0;
@@ -12,9 +22,37 @@ class AppConstants {
   // Navigation constants
   static const int defaultNavigationIndex = 0;
 
+  // Theme detection with respect to user settings
   bool isLight(BuildContext context) {
-    final brightness = MediaQuery.platformBrightnessOf(context);
+    // First try to get from cached settings
+    final useSystemTheme = _cachedUseSystemTheme;
+    final isDarkMode = _cachedIsDarkMode;
 
-    return brightness == Brightness.light;
+    if (useSystemTheme == true) {
+      // If using system theme, check the system brightness
+      final brightness = MediaQuery.platformBrightnessOf(context);
+      return brightness == Brightness.light;
+    } else {
+      // Otherwise use the manual setting
+      return isDarkMode !=
+          true; // If isDarkMode is null or false, return true (light theme)
+    }
+  }
+
+  // Cache for theme preferences to avoid excessive SharedPreferences reads
+  bool? _cachedUseSystemTheme;
+  bool? _cachedIsDarkMode;
+
+  // Initialize cache from SharedPreferences
+  Future<void> initThemeCache() async {
+    final prefs = await SharedPreferences.getInstance();
+    _cachedUseSystemTheme = prefs.getBool(useSystemThemeKey);
+    _cachedIsDarkMode = prefs.getBool(isDarkModeKey);
+  }
+
+  // Update cache when settings change
+  void updateThemeCache({bool? isDarkMode, bool? useSystemTheme}) {
+    if (isDarkMode != null) _cachedIsDarkMode = isDarkMode;
+    if (useSystemTheme != null) _cachedUseSystemTheme = useSystemTheme;
   }
 }
